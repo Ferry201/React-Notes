72d4a3b1348b353369773bd49d9b47c69dc6a9c0
Mia <liqunzhang3@gmail.com>
2025-04-03 22:27:41 +0800

    add : site logo and start button and topbanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import React , { Component } from 'react';
 import { TestLottie } from './Lottie-Component';
+// import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import NotesApp from '@src/Home/note';
 import { ForestNoteWebsitePage } from './Home/ForestNoteWebsitePage';
@@ -10,10 +11,10 @@ export const App = () => {
 	if( location.pathname === '/lottie' ) {
 		return <TestLottie />;
 	}
-	
+	const basename = window.location.hostname === 'localhost' ? '/' : '/React-Notes';
 	// return <NotesApp />;
 	return <>
-	<Router basename="/React-Notes">
+	<Router basename={basename}>
 		<div>
 			<Routes>
 				<Route path='/'  element={<ForestNoteWebsitePage/>} ></Route>
--- src/Home/ForestNoteWebsitePage.js
+++ src/Home/ForestNoteWebsitePage.js
@@ -1,5 +1,6 @@
 import React , { useState , useRef , useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import forestNoteSiteIcon from'./img-collection/forestNote-site-leaf-icon.png'
 import './note.css';
 
 const ForestNoteWebsitePage=()=>{
@@ -8,33 +9,68 @@ const ForestNoteWebsitePage=()=>{
 	const handleGetStarted = () => {
 		navigate('/ForestNote'); // 点击按钮时跳转
 	};
+	
 	return <div className = 'forestNote-app-site-page'>
 		
 		<header className = "topbar">
-			<div className="site-name">ForestNote</div>
-			<nav>
+			<div className='top-nav-container'>
+				<div className='brand'>
+					<img
+						className = "brand-logo"
+						src = { forestNoteSiteIcon }
+						alt = ""
+					/>
+					<div className = "brand-name">ForestNote</div>
+				</div>
+				<nav>
 					
-					<span className='nav-item'>
+					<span className = "nav-item">
 						<a href = "#home">Home</a>
 					</span>
-					<span className='nav-item'>
+					<span className = "nav-item">
 						<a href = "#features">Features</a>
 					</span>
-					<span className='nav-item'>
-						<a href = "#pricing">Pricing</a>
+					<span className = "nav-item">
+						<a href = "#help">Help</a>
 					</span>
-					<span className='nav-item'>
+					<span className = "nav-item">
+						<a href = "#about">About</a>
+					</span>
+					<span className = "nav-item">
 						<a href = "#contact">Contact Us</a>
 					</span>
-			</nav>
+				</nav>
+			</div>
 		</header>
 		
-		<section id = "home" className='section'>🏡 Home Content
-		<div onClick={handleGetStarted}>start</div>
+		<section
+			id = "home"
+			className = "section"
+		>
+			{/*<div className = "slogan">Your Mind, A Growing Forest</div>*/}
+			<div className = "slogan">Take Notes,
+			                          <br />
+			                          Build Your Own Thought Forest</div>
+			{/*<div className = "slogan">A Quiet Place for Your Thoughts to Settle</div>*/}
+			{/*<div className = "slogan">A Living Notebook, A Thriving Mind</div>*/}
+			
+			
+			{/*<div className = "slogan-explanation">Plant Your Thoughts, Watch Them Grow</div>*/}
+			<div className = "slogan-explanation">Write, plan, and stay organized with powerful note-taking and to-do lists.</div>
+			
+			<div className='home-section-footer-start-button'>
+				<div
+					onClick = { handleGetStarted }
+					className = "get-started-button"
+				>Start for Free
+				</div>
+			</div>
 		</section>
-		<section id = "features" className='section'>🚀 Features Content</section>
-		<section id = "pricing" className='section'>💰 Pricing Content</section>
-		<section id = "contact" className='section'>📩 Contact Us Content</section>
+		
+		{/*<section id = "features" className='section'> Features Content</section>*/}
+		{/*<section id = "help" className='section'> Q&A</section>*/}
+		{/*<section id = "about" className='section'> About</section>*/}
+		{/*<section id = "contact" className='section'> Contact Us Content</section>*/}
 		
 		
 	</div>
